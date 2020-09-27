f23a7d2f08bfb18ab5aefaddda97003d6492933a
repl.it user <replituser@example.com>
2020-09-27 04:50:11 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -112,10 +112,6 @@ void * firstMap(HashMap * map) {  //si el mapa esta vacio no sirve
 
 void * nextMap(HashMap * map) {
   int p=map->current+1;
-  while(map->buckets[p]==NULL || map->buckets[p]->key==NULL) {
-    p++;
-    if(p==map->capacity) p=0;
-  }
   map->current=p;
   return map->buckets[p]->value;
 }
