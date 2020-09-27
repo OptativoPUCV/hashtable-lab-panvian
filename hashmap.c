b92acc385cba494b71e9b4cee5f1656c35f65486
repl.it user <replituser@example.com>
2020-09-27 00:05:37 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -46,12 +46,14 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value) {
   unsigned long p=hash(key,map->capacity);
-  while(map->buckets[p]!=NULL && is_equal(map->buckets[p]->key,key)!=0){
+  struct Pair *c=(struct Pair*) malloc(sizeof(struct Pair));
+  while(map->buckets[p]!=NULL && is_equal(map->buckets[p]->key,key)==0){
     p++;
     if(p>map->capacity) p=0;
   }
-  strcpy(map->buckets[p]->key,key);
+  strcpy(c->key,key);
   return;
+  map->buckets[p]=c;
 }
 
 void enlarge(HashMap * map) {
